b07bdefae2dc10c71867187daeefcbe82eadd45a
luis.tallafigo <luis.tallafigo@WKSALMPBB624.u-tad.es>
2016-10-14 10:36:20 +0200

    Commit 6

--- src/Maquina/Controlador.java
+++ src/Maquina/Controlador.java
@@ -3,9 +3,11 @@ package Maquina;
 public class Controlador {
 
 	private static int saldo;
-
+	
+	//Agregamos el dinero al saldo y notificamos el saldo que llevamos
 	private void insertMon(int cant) {
 		saldo = cant;
+		Vista.MSaldo(cant);
 	}
 	
 	//Lo utilizamos para mostrar el dinero que vamos a devolver, y para poner el "Monedero" a 0
@@ -15,12 +17,12 @@ public class Controlador {
 		saldo = 0;
 		return cant;
 	}
-
+	
+	//Provamos a vender un producto, y actualizamos el saldo actual y lo mostramos por pantalla.
 	private void SelPro(String producto, int cantidad) {
-
 		Dispensador.dispensar(producto, saldo,cantidad);
 		saldo=Dispensador.resto();
-
+		Vista.MSaldo(saldo);
 	}
 
 }
--- src/Maquina/Tecla.java
+++ src/Maquina/Tecla.java
@@ -1,24 +1,24 @@
 package Maquina;
 
 import java.util.Scanner;
-/** @autor Luis Martín Tallafigo Gonzalez*/
+
+/** @autor Luis Martín Tallafigo Gonzalez */
 public class Tecla {
-	
+
 	protected int boton = -1;
-	
-	protected void pulsar(){
-		
-		
-		while (boton!=0 || boton!=1 ||boton!=2) {
+
+	// Esta clase hay que cambiarla completamente para poder adaptala a
+	// distintos proveedores(es decir cuando saquemos la informacion de los
+	// proveedores de un documento de txto, una BBDD, etc)
+	protected void pulsar() {
+
+		while (boton != 0 || boton != 1 || boton != 2) {
 			System.out.println("pulse 0 para devolver cambio, 1 para Coca-cola, 2 para Fanta");
 			Scanner scan = new Scanner(System.in);
-			boton= scan.nextInt();
-			
-			
+			boton = scan.nextInt();
+
 		}
-		
-		
-		
+
 	}
 
 }
