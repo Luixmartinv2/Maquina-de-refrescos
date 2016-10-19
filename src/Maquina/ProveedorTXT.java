99994e83ad854607cec82977a5fcf3100a36426f
luis.tallafigo <luis.tallafigo@WKSALMPBB624.u-tad.es>
2016-10-19 12:03:24 +0200

    coomit 13 ProveedorTXT ya puede manejar productos y precios de cualquier tamaño

--- src/Maquina/ProveedorTXT.java
+++ src/Maquina/ProveedorTXT.java
@@ -11,10 +11,12 @@ import java.util.ArrayList;
 public class ProveedorTXT {
 	
 	/***********************************************************/
-	private static void escribirFichero(ArrayList<String> listaProductos) {
+	private static void escribirFichero(ArrayList<String> listaProductos, ArrayList<Integer> listaPrecios) {
 
 		FileWriter fichero = null;
 		PrintWriter pw = null;
+		FileWriter fichero2 = null;
+		PrintWriter pw2 = null;
 
 		try {
 			fichero = new FileWriter("ListaProductos.txt");
@@ -30,9 +32,24 @@ public class ProveedorTXT {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		try {
+			fichero2 = new FileWriter("ListaPrecios.txt");
+
+			for (int x = 0; x <= listaPrecios.size() - 1; x++) {
+
+				pw2 = new PrintWriter(fichero2);
+				pw2.println(listaPrecios.get(x));
+			}
+			fichero2.close();
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 /**********************************************************************/
-	private ArrayList<String> leerFichero() {
+	private ArrayList<String> leerFicheroDeProductos() {
 		ArrayList<String> listaProductos = new ArrayList<String>();
 		File archivo = null;
 
@@ -57,4 +74,28 @@ public class ProveedorTXT {
 		return listaProductos;
 	}
 /**********************************************************************/
+	private ArrayList<Integer> leerFicheroDePrecios() {
+		ArrayList<Integer> listaPrecios = new ArrayList<Integer>();
+		File archivo = null;
+
+		// Recorre el fichero y añadir elementos a mi arraylist
+
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			archivo = new File("ListaPrecios.txt");
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+
+			Integer Linea;
+			while ((Linea = br.read()) != null) {
+				listaPrecios.add(Linea);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return listaPrecios;
+	}
 }
