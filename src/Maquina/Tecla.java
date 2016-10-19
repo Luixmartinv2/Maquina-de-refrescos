e801bf22bad616663ac2f48b715ae8c6ebacf8ca
luis.tallafigo <luis.tallafigo@WKSALMPBB624.u-tad.es>
2016-10-19 12:28:09 +0200

    coomit 14 Todas las clases creadas

--- src/Maquina/Tecla.java
+++ src/Maquina/Tecla.java
@@ -1,19 +1,16 @@
 package Maquina;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /** @autor Luis Martín Tallafigo Gonzalez */
 public class Tecla {
-
-	protected int boton = -1;
-
-	// Esta clase hay que cambiarla completamente para poder adaptala a
-	// distintos proveedores(es decir cuando saquemos la informacion de los
-	// proveedores de un documento de txto, una BBDD, etc)
-	protected void pulsar() {
-
+	protected int boton;
+	// Cambiamos la clase a una secundaria, ya que vamos a crear
+	protected void pulsar(int boton, ArrayList<String> Productos) {
+		this.boton=boton;
 		while (boton != 0 || boton != 1 || boton != 2) {
-			System.out.println("pulse 0 para devolver cambio, 1 para Coca-cola, 2 para Fanta");
+			System.out.println("pulse 0 para devolver cambio, 1 para "+Productos.get(0)+" 2 para "+Productos.get(1));
 			Scanner scan = new Scanner(System.in);
 			boton = scan.nextInt();
 
